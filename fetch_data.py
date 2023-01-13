3fd7acb7016f5e8ca5bca3dde0ca7fb27115acf1
Gurkirat Singh <tbhaxor@gmail.com>
2023-01-13 20:07:36 +0530

    multi threading in the file downloader

--- fetch_data.py
+++ fetch_data.py
@@ -8,6 +8,7 @@ Created on Mon Jan  9 20:28:15 2023
 
 from dotenv import load_dotenv
 
+from concurrent.futures import ThreadPoolExecutor
 import requests
 import os
 from urllib.parse import urljoin
@@ -21,6 +22,7 @@ load_dotenv()
 # download chunk size, # bytes to download at a time
 CHUNK_SIZE = int(os.getenv('CHUNK_SIZE', '4096'))
 BASE_URL = os.getenv('WEB_SERVER_BASE_URL', 'http://localhost:8000')
+PARALLEL_JOBS = int(os.getenv('PARALLEL_DOWNLOAD_JOBS', '3'))
 
 
 def fetch(urls: List[Tuple[str, Path]], base_dir: Path):
@@ -34,16 +36,26 @@ def fetch(urls: List[Tuple[str, Path]], base_dir: Path):
         if not save_path.exists():
             downlodable_urls.append((url, save_path))
 
-    # iterative download and show progress bar using tqdm
-    for url, save_path in downlodable_urls:
-        url = urljoin(base=BASE_URL, url=url)
-
+    # unit job of downloading and saving file
+    def download_job(url, save_path):
         response = requests.get(url, stream=True)
         file_size = int(response.headers.get('content-length', 0))
-        progress = tqdm(desc=save_path.name, total=file_size, ascii=True, unit_scale=True, unit='iB')
+        progress = tqdm(desc=save_path.name,
+                        total=file_size,
+                        ascii=True,
+                        unit_scale=True,
+                        unit='iB',
+                        leave=False)
 
         with open(save_path, 'wb') as file:
             for data in response.iter_content(chunk_size=CHUNK_SIZE):
                 progress.update(len(data))
                 file.write(data)
             progress.close()
+            response.close()
+
+    with ThreadPoolExecutor(max_workers=PARALLEL_JOBS, thread_name_prefix='cloudy_dnldr') as pool:
+        # iterative download and show progress bar using tqdm
+        for url, save_path in downlodable_urls:
+            url = urljoin(base=BASE_URL, url=url)
+            pool.submit(download_job, url, save_path)
