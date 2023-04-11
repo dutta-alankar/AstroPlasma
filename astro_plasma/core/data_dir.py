387f7b916b74a56f0e1dd5185f89c1acf34e6ff1
dutta-alankar <dutta.alankar@gmail.com>
2023-04-11 12:37:05 +0530

    [STY] Black fixes

--- astro_plasma/core/data_dir.py
+++ astro_plasma/core/data_dir.py
@@ -8,12 +8,14 @@ Created on Tue Apr 11 11:09:17 2023
 from typing import Optional, Protocol
 from pathlib import Path
 
+
 # For now this class is redundant, but can be useful later!
 class DataDir(Protocol):
     @property
     def base_dir(self: "DataDir"):
         pass
-    
+
+
 def set_base_dir(DEFAULT_BASE_DIR: Path, base_dir: Optional[Path] = None) -> Path:
     if base_dir is None:
         _base_dir = DEFAULT_BASE_DIR
@@ -22,5 +24,5 @@ def set_base_dir(DEFAULT_BASE_DIR: Path, base_dir: Optional[Path] = None) -> Pat
 
     if not _base_dir.exists():
         _base_dir.mkdir(mode=0o755, parents=True)
-        
+
     return _base_dir
